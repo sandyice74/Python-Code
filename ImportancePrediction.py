e2cae541daf8c5fb861266941cd1b4f792303b8e
Di Sha <dsha@deepforestmedia.com>
2016-01-22 17:14:24 -0800

    update

---
+++ ImportancePrediction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 16:39:55 2015
+CTR prediction feature importance 
+@author: ar-di.sha
+"""
+import numpy as np
+import pandas as pd
+from sklearn import cross_validation
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import random
+
